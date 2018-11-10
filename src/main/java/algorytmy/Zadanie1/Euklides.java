42df8b422dbbbfc1d42f478b6eb7793184eca0bb
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2018-11-10 18:47:48 +0100

    Stworzony pakiet z zadaniami z algorytmow Zadanie 1 - algorytm Euklidesa

---
+++ src/main/java/algorytmy/Zadanie1/Euklides.java
@@ -0,0 +1,33 @@
+package algorytmy.Zadanie1;
+
+import java.util.Scanner;
+
+//    i -> od uzytkownika;
+//    j -> od uzytkownika;
+//    dopoki i-j != 0
+//       jeżeli ( i > j)
+//           i = i - j
+//       jeżeli nie
+//           j = j - i
+//
+
+public class Euklides {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj pierwszą liczbę");
+        int liczba1 = scanner.nextInt();
+        System.out.println("Podaj drugą liczbę");
+        int liczba2 = scanner.nextInt();
+
+        if (liczba1 < 0 || liczba2 < 0) throw new IllegalArgumentException();
+
+        while(liczba1 != liczba2) {
+            if (liczba1 > liczba2){
+                liczba1 -= liczba2;
+            } else {
+                liczba2 -= liczba1;
+            }
+        }
+        System.out.println(liczba1);
+    }
+}
