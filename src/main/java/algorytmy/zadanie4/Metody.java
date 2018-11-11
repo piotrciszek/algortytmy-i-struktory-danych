a715581dd7bd8d2cd69cb73c7b2aacb0454273d6
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2018-11-11 18:05:05 +0100

    Zadanie4 dodanie if a > b

--- src/main/java/algorytmy/zadanie4/Metody.java
+++ src/main/java/algorytmy/zadanie4/Metody.java
@@ -2,13 +2,29 @@ package algorytmy.zadanie4;
 
 public class Metody {
     public void pokazLiczby(int a, int b){
-        for (int i=a; i<=b; i++){
-            System.out.print(i);
-            if (i < b){
-                System.out.print(", ");
-            } else {
-                System.out.print(".");
+        int m = a;
+        int n = b;
+        if (a < b) {
+            for (int i = a; i <= b; i++) {
+                System.out.print(i);
+                if (i < b) {
+                    System.out.print(", ");
+                } else {
+                    System.out.print(".");
+                }
             }
+        } else {
+            a = n;
+            b = m;
+            for (int i = a; i <= b; i++) {
+                System.out.print(i);
+                if (i < b) {
+                    System.out.print(", ");
+                } else {
+                    System.out.print(".");
+                }
+            }
+
         }
     }
 }
